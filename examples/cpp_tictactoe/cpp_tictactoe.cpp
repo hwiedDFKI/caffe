36d6b1f4607bf42eb8edf5b1cc346521b9233c02
shi-yan <billconan@gmail.com>
2016-10-27 00:42:39 -0700

    speed up tictactoe

--- examples/cpp_tictactoe/cpp_tictactoe.cpp
+++ examples/cpp_tictactoe/cpp_tictactoe.cpp
@@ -1,5 +1,6 @@
 #include <caffe/caffe.hpp>
 #include <memory>
+#include <map>
 #include "caffe/layers/memory_data_layer.hpp"
 
 static double rate = 0.1;
@@ -24,27 +25,41 @@ static bool isWin(int position[9], int side)
     }
 }
 
-float getReward(std::shared_ptr<caffe::Net<float> > &testnet, std::vector<float> &position, int side, int depth)
+void getReward(std::shared_ptr<caffe::Net<float> > &testnet, std::vector<float> &position, std::vector<float> &label)
 {
     caffe::MemoryDataLayer<float> *dataLayer_testnet = (caffe::MemoryDataLayer<float> *) (testnet->layer_by_name("test_inputdata").get());
-    float fakelabel = 0.0f;
-    dataLayer_testnet->Reset(&position[0], &fakelabel, 1);
+    dataLayer_testnet->Reset(&position[0], &label[0], 5477);
 
     testnet->Forward();
 
     boost::shared_ptr<caffe::Blob<float> > output_layer = testnet->blob_by_name("output");
 
     const float* q = output_layer->cpu_data();
-    
-    return *q;
+
+    memcpy(&label[0], q, sizeof(float) * 5477);
 }
 
-
-void recursiveTrain(std::shared_ptr<caffe::Net<float> > &testnet, std::vector<float> &data, std::vector<float> &label, int position[9], int side, int depth)
+unsigned int keyForPosition(int position[9])
 {
-    for(int i = 0;i<9;++i)
+    unsigned int result = 0;
+
+    result |= (position[0] + 1) << 16;
+    result |= (position[1] + 1) << 14;
+    result |= (position[2] + 1) << 12;
+    result |= (position[3] + 1) << 10;
+    result |= (position[4] + 1) << 8;
+    result |= (position[5] + 1) << 6;
+    result |= (position[6] + 1) << 4;
+    result |= (position[7] + 1) << 2;
+    result |= (position[8] + 1) << 0;
+
+    return result;
+}
+
+void preGeneratePermutations(unsigned int &id, std::vector<float> &data, std::vector<float> &label, std::map<unsigned int, unsigned int> &hash, int position[0], int side, int depth)
+{
+    for(int i = 0; i < 9; ++i)
     {
-        //if ((depth == 1 && i == 4) || (depth == 3 && (i == 5 || i==3 || i==1 || i==7)) || (depth != 1 && depth !=3))
         if (position[i] == 0)
         {
             position[i] = side;
@@ -55,75 +70,123 @@ void recursiveTrain(std::shared_ptr<caffe::Net<float> > &testnet, std::vector<fl
             if (isWin(position,  side))
             {
                 hasWon = true;
-                if (side == 1)
-                {
-                    reward = 1.0; //std::min(1.0, 0.5 + 0.05 * (18-depth));
-                }
-                else
-                {
-                    reward = -1.0; //std::max(0.0, 0.5 - 0.05 * (18-depth));
-                }
             }
-            else
+
+            unsigned int key = keyForPosition(position);
+
+            //printf("key:%d\n", key);
+
+            if (hash.find(key) == hash.end())
             {
-                for(int k=0; k<9; ++k)
-                {
-                    if (position[k] !=0)
-                    {
-                        continue;
-                    }
-                    std::vector<float> inputs;
-
-                    for(int e = 0;e<9;++e)
-                    {
-                        inputs.push_back(position[e]);
-                    }
-                    inputs[k] = -side;
-
-                    float q = getReward(testnet, inputs, -side, depth + 1);
-
-                    if (notset)
-                    {
-                        notset = false;
-                        reward = q;
-                    }
-                    else if (side == 1)
-                    {
-                        if (q < reward)
-                        {
-                            reward = q;
-                        }
-                    }
-                    else
-                    {
-                        if (q > reward)
-                        {
-                            reward = q;
-                        }
-                    }
-                }
-                reward = 0.9 * reward;
+                hash[key] = id;
+                label.push_back(reward);
+                data.push_back(position[0]);
+                data.push_back(position[1]);
+                data.push_back(position[2]);
+                data.push_back(position[3]);
+                data.push_back(position[4]);
+                data.push_back(position[5]);
+                data.push_back(position[6]);
+                data.push_back(position[7]);
+                data.push_back(position[8]);
+                id ++;
             }
-
-            //reward = (reward + 2.0 ) * 0.5
-
-            label.push_back(reward);
-            data.push_back(position[0]);
-            data.push_back(position[1]);
-            data.push_back(position[2]);
-            data.push_back(position[3]);
-            data.push_back(position[4]);
-            data.push_back(position[5]);
-            data.push_back(position[6]);
-            data.push_back(position[7]);
-            data.push_back(position[8]);
 
             if (!hasWon)
             {
-                recursiveTrain(testnet, data, label, position, -side, depth +1);
+                preGeneratePermutations(id, data, label, hash,position, -side, depth +1);
             }
+
             position[i] = 0;
         }
+    }
+}
+
+
+void getTrainingData(std::map<unsigned int, unsigned int> &hash, 
+                    std::vector<float> &oldLabel, 
+                    std::vector<float> &data, std::vector<float> &label)
+{
+    int position[9] = {0};
+    for(int i = 5477-1; i>=0; i--)
+    {
+        float reward = 0.0;
+        bool notset = true;
+        bool hasWon = false;
+
+        int posC = 0;
+
+        for(int e = 0;e<9;++e)
+        {
+
+            position[e] = data[i*9 + e];
+            if (position[e] == 1)
+            {
+                posC ++;
+            }
+            else if (position[e] == -1)
+            {
+                posC --;
+            }
+        }
+
+        int side = -1;
+
+        if (posC > 0)
+            side = 1;
+            
+        if (isWin(position,  1))
+        {
+            hasWon = true;
+            reward = 1.0; //std::min(1.0, 0.5 + 0.05 * (18-depth));
+        }
+        else if(isWin(position, -1))
+        {
+            hasWon = true;
+            reward = -1.0; //std::max(0.0, 0.5 - 0.05 * (18-depth));
+        }
+        else
+        {
+            for(int k=0; k<9; ++k)
+            {
+                if (position[k] !=0)
+                {
+                    continue;
+                }
+    
+                int inputs[9];
+                memcpy(inputs, position, sizeof(int) * 9);
+
+                inputs[k] = -side;
+
+                unsigned int inputKey = keyForPosition(&inputs[0]);
+                unsigned int id = hash[inputKey];
+                float q = oldLabel[id];
+
+                if (notset)
+                {
+                    notset = false;
+                    reward = q;
+                }
+                else if (side == 1)
+                {
+                    if (q < reward)
+                    {
+                        reward = q;
+                    }
+                }
+                else
+                {
+                    if (q > reward)
+                    {
+                        reward = q;
+                    }
+                }
+            }
+            reward = 0.9 * reward;
+        }
+
+        label[i] = reward;
     }
 }
 
@@ -140,20 +203,27 @@ int main()
     testnet.reset(new caffe::Net<float>("./model.prototxt", caffe::TEST));
     testnet->ShareTrainedLayersWith(solver->net().get());
 
+    std::vector<float> data;
+    std::vector<float> label;
+    std::vector<float> oldLabel;
+    std::map<unsigned int, unsigned int> hash;
+    unsigned int id = 0;
+    int position[9] = {0};
+
+    preGeneratePermutations(id, data, label, hash, position, 1, 1);
+    oldLabel.resize(label.size(), 0);
+
+    printf("data size: %d label size: %d hash size: %d\n", data.size(), label.size(), hash.size());
+
 
     for(int i = 0; i < 5000000; ++i)
     {
-        std::vector<float> data;
-        data.reserve(549945 * 9);
-        std::vector<float> label;
-        label.reserve(549945);
-        int position[9] = {0};
 
-        recursiveTrain(testnet, data, label, position, 1, 1);
+        getReward(testnet, data, oldLabel);
 
-        printf("%d data_size: %d, label_size: %d\n",i, data.size(), label.size());
+        getTrainingData(hash, oldLabel, data, label);
 
-        dataLayer_trainnet->Reset(&data[0], &label[0], 549945);
+        dataLayer_trainnet->Reset(&data[0], &label[0], 5477);
 
         solver->Step(1);
     }
