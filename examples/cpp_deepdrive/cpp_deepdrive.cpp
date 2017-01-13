4fabea84ff738bf4a1794ef6a064e9dd60625177
Shi Yan <shiy@nvidia.com>
2017-01-12 17:25:58 -0800

    add deepdrive

--- examples/cpp_deepdrive/cpp_deepdrive.cpp
+++ examples/cpp_deepdrive/cpp_deepdrive.cpp
@@ -3,7 +3,7 @@
 #include "caffe/layers/memory_data_layer.hpp"
 #include "endian.h"
 
-static FILE *datafp = 0;
+/*static FILE *datafp = 0;
 static FILE *labelfp = 0;
 static unsigned int numOfImage = 0;
 static unsigned int numOfRow = 0;
@@ -53,9 +53,24 @@ void closeData()
     fclose(datafp);
     fclose(labelfp);
 }
+*/
+
+ #include <sys/time.h>
+
+    typedef unsigned long long timestamp_t;
+
+    static timestamp_t
+    
+    get_timestamp ()
+    {
+      struct timeval now;
+      gettimeofday (&now, NULL);
+      return  now.tv_usec + (timestamp_t)now.tv_sec * 1000000;
+    }
 
 int main()
 {
+    caffe::set_mode(caffe::GPU);
 	/*float *data = new float[64*1*1*3*400];
 	float *label = new float[64*1*1*1*400];
 
@@ -70,18 +85,54 @@ int main()
 		label[i] = c;
 	}*/
 
-    caffe::SolverParameter solver_param;
-    caffe::ReadSolverParamsFromTextFileOrDie("./solver.prototxt", &solver_param);
+    //caffe::SolverParameter solver_param;
+    //caffe::ReadSolverParamsFromTextFileOrDie("./solver.prototxt", &solver_param);
 
-    boost::shared_ptr<caffe::Solver<float> > solver(caffe::SolverRegistry<float>::CreateSolver(solver_param));
-    caffe::MemoryDataLayer<float> *dataLayer_trainnet = (caffe::MemoryDataLayer<float> *) (solver->net()->layer_by_name("inputdata").get());
+    //boost::shared_ptr<caffe::Solver<float> > solver(caffe::SolverRegistry<float>::CreateSolver(solver_param));
+    //caffe::MemoryDataLayer<float> *dataLayer_trainnet = (caffe::MemoryDataLayer<float> *) (solver->net()->layer_by_name("inputdata").get());
    // caffe::MemoryDataLayer<float> *dataLayer_testnet_ = (caffe::MemoryDataLayer<float> *) (solver->test_nets()[0]->layer_by_name("test_inputdata").get());
+    boost::shared_ptr<caffe::Net<float> > testnet;
+    testnet.reset(new caffe::Net<float>("./deep_drive_model.prototxt", caffe::TEST));
+
+    printf("======================= load model\n");
+
+    testnet->CopyTrainedLayersFrom("./caffe_deep_drive_train_iter_35352.caffemodel");
+
+    printf("======================= load weight\n");
+
+    
+    caffe::Blob<float>* input_layer = testnet->input_blobs()[0];
+    int num_channels_ = input_layer->channels();
+    int width = input_layer->width();
+    int height = input_layer->height();
+
+    printf("num_channels:%d\n", num_channels_);
+    printf("width:%d\n", width);
+    printf("height:%d\n", height);
+
+
+
+
+    // Process
+
+
+
+    printf("test forward\n");
+    timestamp_t t0 = get_timestamp();
+    testnet->Forward();
+    timestamp_t t1 = get_timestamp();
+    double secs = (t1 - t0) / 1000000.0L;
+    printf("forward done:%f\n",secs);
+
+
+
+
 
   //  float testab[] = {0, 0, 0, 1, 1, 0, 1, 1};
   //  float testc[] = {0, 1, 1, 0};
 
   //  dataLayer_testnet_->Reset(testab, testc, 4);
-
+/*
     openData();
 
     float *data = new float[numOfImage * numOfRow * numOfColumn];
@@ -120,7 +171,7 @@ int main()
     solver->Solve();
 
     closeData();
-
+*/
     return 0;
 /*
     boost::shared_ptr<caffe::Net<float> > testnet;
